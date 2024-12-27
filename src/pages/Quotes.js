553f32d4aa69631f6951c4b94d83b7ac536daf93
عَli Taha <61361384+gedankener@users.noreply.github.com>
2024-12-27 15:21:49 -0500

    Update Quotes.js

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -7,7 +7,7 @@ const Quotes = ({ isChaos }) => {
       <h2>Favorite Quotes</h2>
       <blockquote>"He never created a finished product. Finished products are for decadent minds. His was an evolving mechanism.." - Asimov</blockquote>
       <blockquote>"You have to go over the line to see the line" - Simpkins</blockquote>
-      <blockquote>"Quote 3" - Author</blockquote>
+      <blockquote>"Human Relationship Defy Catogarization" - Author</blockquote>
     </>
   );
 
